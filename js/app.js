3ad7fdb2ee14e40680d97166bd5beb2f28b9edaf
Xsorter <xsorter@gmail.com>
2018-01-11 00:20:55 +0200

    add button elements, display values at result input

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
 
 
+
 let data = {
-    numbersArray : [],
+    buttonsArray : [0,1,2,3,4,5,6,7,8,9,'+','-','/','*','(',')','='],
     mainDOM : document.querySelector('#wrapper'),
+    resultDOM: document.querySelector('#calculation-result'),
+   
 }
 
 function init(){
-    data.mainDOM.insertAdjacentHTML('beforeend', `
-        <div>init</div>
-    `);
+    for(let i=0; i<data.buttonsArray.length; i++){
+        data.mainDOM.insertAdjacentHTML('beforeend', `
+            <input 
+                class="button button__${data.buttonsArray[i]}" 
+                readonly value="${data.buttonsArray[i]}
+                
+            ">
+        `);
+    }
 }
 
 init();
+
+let arrButtons = document.querySelectorAll('.button');
+
+console.log(arrButtons);
+
+for(let i=0; i<arrButtons.length; i++){
+    arrButtons[i].addEventListener('click', function(){
+        data.resultDOM.value += arrButtons[i].value.trim()
+        console.log(arrButtons[i].value)
+    })
+}
+
