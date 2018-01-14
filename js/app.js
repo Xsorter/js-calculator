3f7a50c2677383f3154b8cf15f9536ac8db0183c
Xsorter <xsorter@gmail.com>
2018-01-14 19:32:00 +0200

    add events on '=' and 'C' buttons, read value from result input

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 
 let data = {
-    buttonsArray : [0,1,2,3,4,5,6,7,8,9,'+','-','/','*','(',')','='],
+    buttonsArray : ["C",0,1,2,3,4,5,6,7,8,9,'+','-','/','*','(',')','='],
     mainDOM : document.querySelector('#wrapper'),
     resultDOM: document.querySelector('#calculation-result'),
    
@@ -28,8 +28,12 @@ console.log(arrButtons);
 
 for(let i=0; i<arrButtons.length; i++){
     arrButtons[i].addEventListener('click', function(){
-        if(arrButtons[i].value.trim() !== "="){
+        if(arrButtons[i].value.trim() !== "=" && arrButtons[i].value.trim() !== "C"){
             data.resultDOM.value += arrButtons[i].value.trim()
+        }else if(arrButtons[i].value.trim() === "C"){
+            data.resultDOM.value = " "    
+        }else if(arrButtons[i].value.trim() === "="){    
+            parseCalculation(data.resultDOM.value.trim())
         }
         console.log(arrButtons[i].value)
     })
@@ -41,17 +45,21 @@ for(let i=0; i<arrButtons.length; i++){
 function parseCalculation(val) {
     let signs = ["*", "+"];         // signs in the order in which they should be evaluated
     let funcs = [multiply, add];                 // the functions associated with the signs
-    let tokens = str.split(/\b/);      // split the string into "tokens" (numbers or signs)
+    let tokens = val.split(/\b/);      // split the string into "tokens" (numbers or signs)
 
     //run through two-dimentional array
     for (let round = 0; round < signs.length; round++) {          // do this for every sign
+
         console.log("tokens at this point: " + tokens.join(" "));
+
         for (let place = 0; place < tokens.length; place++) {    // do this for every token
             if (tokens[place] == signs[round]) {                         // a sign is found
                 let a = parseInt(tokens[place - 1]);    // convert previous token to number
                 let b = parseInt(tokens[place + 1]);        // convert next token to number
                 let result = funcs[round](a, b);           
                 console.log("calc: " + a + signs[round] + b + "=" + result);
+
+                data.resultDOM.value = result;
                 tokens[place - 1] = result.toString();      // store the result as a string
                 tokens.splice(place--, 2);  // delete obsolete tokens and back up one place
             }
@@ -59,7 +67,6 @@ function parseCalculation(val) {
     }
 
     return tokens[0];  
-
     function multiply(x, y) {                  
         return x * y;
     }
@@ -68,5 +75,4 @@ function parseCalculation(val) {
     }
 }
 
-let str = "3*7+8*9";
-console.log("result: " + str + " = " + parseCalculation(str));
+
